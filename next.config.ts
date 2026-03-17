2906d6f23ac8340c07836bb798adebe2e028d00e
ap-ethnicinfotech <ap.ethnicinfotech@gmail.com>
2026-03-17 09:57:33 +0530

    add api end point

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    BACKEND_DOMAIN:'http://192.168.1.180:5003'
+    BACKEND_DOMAIN:'https://api-admin.ethnicinfotech.in'
   }
 };
 
