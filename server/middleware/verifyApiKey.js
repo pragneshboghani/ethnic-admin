2b01755344f0df270a87d59e9facf8be52bf5bb3
nensi khunt <nk.ethnicinfotech@gmail.com>
2026-03-25 11:39:33 +0530

    Add return json for api key'

--- server/middleware/verifyApiKey.js
+++ server/middleware/verifyApiKey.js
@@ -6,6 +6,8 @@ const verifyApiKey = (req, res, next) => {
   if (!apiKey || apiKey !== process.env.API_KEY) {
     return res.status(401).json({
       success: false,
+      apiKey,
+      API_KEY: process.env.API_KEY,
       message: "Unauthorized - Invalid API Key",
     });
   }
