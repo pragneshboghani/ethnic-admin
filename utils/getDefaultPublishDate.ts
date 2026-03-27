bf45c413824289a87549033a7520dd91ad0af1f2
ap-ethnicinfotech <ap.ethnicinfotech@gmail.com>
2026-03-27 12:25:04 +0530

    fix publish date issue

--- utils/getDefaultPublishDate.ts
+++ utils/getDefaultPublishDate.ts
@@ -1,26 +1,11 @@
-const getDefaultPublishDate = (globalStatus: string, publishDate?: string) => {
-  if (publishDate) return publishDate;
-
+const getDefaultPublishDate = (globalStatus: string) => {
   const now = new Date();
 
-  if (globalStatus === "publish" || globalStatus === 'draft') {
-    return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(
-      now.getDate()
-    ).padStart(2, "0")}T${String(now.getHours()).padStart(2, "0")}:${String(
-      now.getMinutes()
-    ).padStart(2, "0")}`;
-  }
-
   if (globalStatus === "future") {
-    now.setDate(now.getDate() + 1);
-    return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(
-      now.getDate()
-    ).padStart(2, "0")}T${String(now.getHours()).padStart(2, "0")}:${String(
-      now.getMinutes()
-    ).padStart(2, "0")}`;
+    now.setUTCDate(now.getUTCDate() + 1);
   }
 
-  return "";
+  return now.toISOString().slice(0, 16);
 };
 
-export default getDefaultPublishDate
+export default getDefaultPublishDate;
