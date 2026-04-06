f4d26b772e0aaf0f0321437b17830392bd20050f
ap-ethnicinfotech <ap.ethnicinfotech@gmail.com>
2026-04-06 09:50:09 +0530

    create seprate component for viewdetail modal for update category

--- app/(dashboard)/account/category/page.tsx
+++ app/(dashboard)/account/category/page.tsx
@@ -2,13 +2,14 @@
 
 import CategoryAndTagAction from '@/actions/categoryAndTagAction';
 import PlateformActions from '@/actions/PlateFormActions';
+import ViewDetailsModal from '@/components/category/ViewDetailsModal';
 import CategoryModal, { PlatformResponse } from '@/components/common/CategoryModal';
 import TagModal from '@/components/common/TagModal';
 import { Eye, Trash2, X } from 'lucide-react';
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 
-type Category = {
+export type Category = {
     id: number;
     name: string;
     description?: string;
@@ -202,45 +203,47 @@ const Page = () => {
                 />
             )}
             {showData.data !== null && (
-                <>
-                    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-                        <div className="p-6 w-[400px] glass-card space-y-4">
-                            <div className="flex justify-between items-center">
-                                <h2 className="text-xl font-semibold">{showData.type === 'category' ? 'Category Details' : 'Tag Details'}</h2>
-                                <button className="text-white" onClick={() => setShowdata({ data: null, type: '' })}>
-                                    <X />
-                                </button>
-                            </div>
-                            <p><strong>Name:</strong> {showData.data.name}</p>
-                            <p><strong>Description:</strong> {showData.data.description || '-'}</p>
-                            <p><strong>Platform:</strong>
-                                {showData.data.platform_ids?.map((platform: any) => {
-                                    const platformInfo = platformData?.data?.find(p => p.id === platform);
-                                    return platformInfo ? (
-                                        <span key={platform} className="text-white px-2 py-1 rounded-full text-md mr-1">
-                                            {platformInfo.platform_name}
-                                        </span>
-                                    ) : null;
-                                })}</p>
-                            <p><strong>Slug:</strong> {showData.data.slug}
-                            </p>
-                            <p>
-                                <strong>Created At:</strong>{' '}
-                                {showData.data.created_at
-                                    ? new Date(showData.data.created_at).toLocaleString()
-                                    : '-'}
-                            </p>
-                            <div className='flex align-self-end justify-end gap-4'>
-                                <button className='btn' onClick={handleEdit}>
-                                    Update
-                                </button>
-                                <button className="btn" onClick={()=>{handleDeleteCategory(showData.data as Category, showData.type === 'category' ? 'category' : 'tags')}}>
-                                    Delete {showData.type === 'category' ? 'Category' : 'Tag'}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </>
+                // <>
+                //     <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+                //         <div className="p-6 w-[400px] glass-card space-y-4">
+                //             <div className="flex justify-between items-center">
+                //                 <h2 className="text-xl font-semibold">{showData.type === 'category' ? 'Category Details' : 'Tag Details'}</h2>
+                //                 <button className="text-white" onClick={() => setShowdata({ data: null, type: '' })}>
+                //                     <X />
+                //                 </button>
+                //             </div>
+                //             <p><strong>Name:</strong> {showData.data.name}</p>
+                //             <p><strong>Description:</strong> {showData.data.description || '-'}</p>
+                //             <p><strong>Platform:</strong>
+                //                 {showData.data.platform_ids?.map((platform: any) => {
+                //                     const platformInfo = platformData?.data?.find(p => p.id === platform);
+                //                     return platformInfo ? (
+                //                         <span key={platform} className="text-white px-2 py-1 rounded-full text-md mr-1">
+                //                             {platformInfo.platform_name}
+                //                         </span>
+                //                     ) : null;
+                //                 })}</p>
+                //             <p><strong>Slug:</strong> {showData.data.slug}
+                //             </p>
+                //             <p>
+                //                 <strong>Created At:</strong>{' '}
+                //                 {showData.data.created_at
+                //                     ? new Date(showData.data.created_at).toLocaleString()
+                //                     : '-'}
+                //             </p>
+                //             <div className='flex align-self-end justify-end gap-4'>
+                //                 <button className='btn' onClick={handleEdit}>
+                //                     Update
+                //                 </button>
+                //                 <button className="btn" onClick={()=>{handleDeleteCategory(showData.data as Category, showData.type === 'category' ? 'category' : 'tags')}}>
+                //                     Delete {showData.type === 'category' ? 'Category' : 'Tag'}
+                //                 </button>
+                //             </div>
+                //         </div>
+                //     </div>
+                // </>
+
+                <ViewDetailsModal showData={showData} setShowdata={setShowdata} platformData={platformData} handleEdit={handleEdit} handleDeleteCategory={handleDeleteCategory} />
             )}
         </>
     )
---
+++ components/category/ViewDetailsModal.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Category } from '@/app/(dashboard)/account/category/page';
+import { X } from 'lucide-react';
+import { Dispatch, SetStateAction } from 'react';
+import { PlatformResponse } from '../common/CategoryModal';
+
+type ViewDetailsModalProps = {
+    showData: {
+        data: Category | null;
+        type: string;
+    };
+    setShowdata: Dispatch<SetStateAction<{
+        data: Category | null;
+        type: string;
+    }>>;
+    platformData: PlatformResponse | null;
+    handleEdit: () => void;
+    handleDeleteCategory: (category: Category, type: string) => void;
+};
+
+const ViewDetailsModal = ({ showData, setShowdata, platformData, handleEdit, handleDeleteCategory }: ViewDetailsModalProps) => {
+
+    if (!showData.data) return null; 
+    return (
+        <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+            <div className="p-6 w-[400px] glass-card space-y-4">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold">{showData.type === 'category' ? 'Category Details' : 'Tag Details'}</h2>
+                    <button className="text-white" onClick={() => setShowdata({ data: null, type: '' })}>
+                        <X />
+                    </button>
+                </div>
+                <p><strong>Name:</strong> {showData.data.name}</p>
+                <p><strong>Description:</strong> {showData.data.description || '-'}</p>
+                <p><strong>Platform:</strong>
+                    {showData.data.platform_ids?.map((platform: any) => {
+                        const platformInfo = platformData?.data?.find(p => p.id === platform);
+                        return platformInfo ? (
+                            <span key={platform} className="text-white px-2 py-1 rounded-full text-md mr-1">
+                                {platformInfo.platform_name}
+                            </span>
+                        ) : null;
+                    })}</p>
+                <p><strong>Slug:</strong> {showData.data.slug}
+                </p>
+                <p>
+                    <strong>Created At:</strong>{' '}
+                    {showData.data.created_at
+                        ? new Date(showData.data.created_at).toLocaleString()
+                        : '-'}
+                </p>
+                <div className='flex align-self-end justify-end gap-4'>
+                    <button className='btn' onClick={handleEdit}>
+                        Update
+                    </button>
+                    <button className="btn" onClick={() => { handleDeleteCategory(showData.data as Category, showData.type === 'category' ? 'category' : 'tags') }}>
+                        Delete {showData.type === 'category' ? 'Category' : 'Tag'}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ViewDetailsModal;
