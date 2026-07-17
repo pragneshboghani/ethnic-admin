5e4f520807fc04f6a94294779fc8db32f50fadbc
E-NensiKhunt <111731490+E-NensiKhunt@users.noreply.github.com>
2026-07-17 19:08:53 +0530

    Revert "fix issue auto save on copy platform token"

--- components/plateform/AddEditPlatformModal.tsx
+++ components/plateform/AddEditPlatformModal.tsx
@@ -409,25 +409,14 @@ const AddEditPlatformModal = ({ open, onClose, editingPlatform, refreshPlatforms
                                                             className={inputClassName}
                                                         />
                                                         {!!watch("platform_token") && (
-                                                            <button className="absolute right-5 bottom-4" 
-                                                            onClick={(e)=>{
-                                                                e.preventDefault();
-                                                                e.stopPropagation();
-                                                                copyPlatformToken();
-                                                            }}
-                                                            type="button"
-                                                            >
+                                                            <button className="absolute right-5 bottom-4" onClick={copyPlatformToken}>
                                                                 <Copy size={18} />
                                                             </button>
                                                         )}
                                                     </div>
                                                 </div>
                                                 <button className="inline-flex items-center justify-center rounded-xl bg-[#eef4ff] px-5 py-3 text-sm font-semibold text-[#0f1724] transition hover:bg-white cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
-                                                    onClick={(e)=> {
-                                                        e.preventDefault();
-                                                        e.stopPropagation();
-                                                        generatePlatformToken();
-                                                    }}
+                                                    onClick={generatePlatformToken}
                                                     disabled={dataSource !== "admin"}
                                                     // !!watch("platform_token") ||
                                                     type="button"
